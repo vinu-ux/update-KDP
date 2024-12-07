05969c0bbd5c8b5cdf4437b2ed7078d790792edd
vinu-ux <vineethes8@gmail.com>
2024-12-07 22:23:28 +0530

    added

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -5,6 +5,8 @@ function clearSection(stat){
         document.getElementById('phone').value  = ''
         document.getElementById('email').value = ''
         document.getElementById('message').value = ''
+        document.getElementById('subject').value = ''
+
     }
     if(stat=='modal'){
         document.getElementById('mod-first').value = '' 
@@ -22,6 +24,7 @@ function sendEmail() {
       phone: document.getElementById('phone').value ,
       email: document.getElementById('email').value,
       message: document.getElementById('message').value,
+      subject: document.getElementById('subject').value
     }
     var htmlBody;
     htmlBody=`<div style="font-family: Arial, sans-serif; color: #333; max-width: 600px; border: 1px solid #e0e0e0; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; background-color: #ffffff;">
@@ -36,7 +39,7 @@ function sendEmail() {
 <p style="margin: 5px 0; color: #7f8c8d; font-size: 14px;"><strong>Email:</strong> ${body.email}</p>
 <p style="margin: 5px 0; color: #7f8c8d; font-size: 14px;"><strong>Phone:</strong> ${body.phone}</p>
 <div style="margin-top: 10px; padding: 5px; background-color: #f9f9f9; border-radius: 5px;">
-  <h6 style="margin: 0 0 5px; color: #34495e; font-size: 18px; font-weight: 500;">Message</h6>
+  <h6 style="margin: 0 0 5px; color: #34495e; font-size: 18px; font-weight: 500;">${body.subject}</h6>
   <p style="margin: 0; color: #555555; font-size: 15px; line-height: 1.6;">${body.message}</p>
 </div>
 </div>`
@@ -99,6 +102,38 @@ function modalEmail() {
         console.error('Error:', error);
     });
 }
+
+function footerEmail() {
+    let email = document.getElementById('ft-email').value
+      
+      var htmlBody;
+      htmlBody=`<div style="font-family: Arial, sans-serif; color: #333; max-width: 600px; border: 1px solid #e0e0e0; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 20px; background-color: #ffffff;">
+    <h2 style="
+    background: #efecec;
+    margin-top: 0px;
+    padding: 10px;
+">${email}</h2>
+      </div>`
+      fetch('/send-email', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ htmlBody })
+      })
+      .then(response => response.text())
+      .then(data => {
+          alert('Email sended Successfully');
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+  }
+
+  document.getElementById('footerEmailButton').onclick = function(e) {
+    e.preventDefault(); // Prevent default button action
+    footerEmail(); // Call the sendEmail function
+};
 
 
 document.getElementById('modalEmailButton').onclick = function(e) {
